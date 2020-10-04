0c6379dbf5655112d90b70f9529513cd2a052d58
TanjaNY <tanja.nyberg@outlook.com>
2020-10-04 16:21:43 +0200

    settings.py database settings changed S3

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'jobs',
+    'upload',
+    'storages',
     
 ]
 
@@ -126,12 +128,28 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+USE_S3 = os.getenv('USE_S3') == 'TRUE'
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+if USE_S3:
+    # aws settings
+    AWS_ACCESS_KEY_ID = os.getenv('AKIAYOTUFMPL27WPBRLD')
+    AWS_SECRET_ACCESS_KEY = os.getenv('jsjgkON7uML1q0jWz/erc4yODh+/4W+cozmMoND')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('elasticbeanstalk-eu-central-1-581137949655')
+    AWS_DEFAULT_ACL = 'public-read'
+    AWS_S3_CUSTOM_DOMAIN = f'elasticbeanstalk-eu-central-1-581137949655.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    # s3 static settings
+    AWS_LOCATION = 'static'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+else:
+    STATIC_URL = '/staticfiles/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_ROOT = BASE_DIR
-MEDIA_URL = '/media/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/mediafiles/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
