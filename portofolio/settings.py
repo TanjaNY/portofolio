d9fe016c437746fd240a3f2e118cff2ed0f90822
TanjaNY <tanja.nyberg@outlook.com>
2020-10-05 14:32:17 +0200

    storage S3

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -48,13 +48,14 @@ AWS_STORAGE_BUCKET_NAME='elasticbeanstalk-eu-central-1-581137949655'
 AWS_S3_REGION_NAME = 'eu-central-1'  # e.g. us-east-2
 AWS_ACCESS_KEY_ID = 'AKIAYOTUFMPLWZRPQFGX'
 AWS_SECRET_ACCESS_KEY = 'rsCV9F5wEtry8nwgLZvsqyXpSuulzyrwPwC+WnqB'
+AWS_CUSTOM_DOMAIN='%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_DEFAULT_ACL= None
 
-# Tell django-storages the domain to use to refer to static files.
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+STATICFILES_LOCATION='static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
-# Tell the staticfiles app to use S3Boto3 storage when writing the collected static files (when
-# you run `collectstatic`).
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIAFILES_LOCATION='media'
+DWFAULT_FILE_STORAGE= 'custom_storages.MediaStorage'
 
 
 
